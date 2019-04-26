a5c42cec963ab7d979e30ed368f5fd74786bac69
Kristján Oddsson <koddsson@github.com>
2019-04-26 13:24:08 +0100

    update script to include category

--- tools/gen-json.js
+++ tools/gen-json.js
@@ -1,20 +1,31 @@
 {
   const { $$, copy } = window // Chrome Console API
 
-  const andrs = $$('td.andr')
-  const trs = andrs.map(e => e.parentElement)
+  const andrs = $$('th.bighead, td.andr')
+  const trs = andrs.map(el => {
+    if (el.tagName === 'TD') {
+      return el.parentElement
+    }
+    return el
+  })
 
-  const emojis = trs.map(tr => {
-    const tds = tr.children
+  let category
+  const emojis = trs.map(el => {
+    if (el.tagName === 'TH') {
+      category = el.innerText
+      return
+    }
+    const tds = el.children
     const codes = tds[1].innerText.replace(/U\+/g, '')
     return {
       no: parseInt(tds[0].innerText),
       codes: codes,
       char: codes.split(' ').map(s => String.fromCodePoint(parseInt(s, 16))).join(''),
       name: tds[3].innerText,
-      keywords: tds[4].innerText
+      keywords: tds[4].innerText,
+      category
     }
-  })
+  }).filter(x => x)
 
   console.log(emojis)
   copy(JSON.stringify(emojis, null, 2))
