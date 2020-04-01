4c4250b4d9efa460f6c2342078d515a48cc1965f
Michael Klaus <michael.klaus@gmx.net>
2020-04-01 22:14:27 +0200

    Added structured json Added version filter to build command (e.g. yarn build 12.1) Always pulling raw data from 13.0

--- scripts/gen.js
+++ scripts/gen.js
@@ -2,14 +2,37 @@ const fs = require('fs')
 const path = require('path')
 const https = require('https')
 
-const EMOJI_VERSION = '12.1'
+const EMOJI_VERSION = '13.0'
 
-main()
+main(process.argv[2])
 
-async function main () {
-  const text = await getTestFile(EMOJI_VERSION)
+function addStructured({structured, group, subgroup}, meta) {
+  const s = structured
+  const data = {
+    version: meta.version,
+    char: meta.char,
+    key: meta.key,
+  }
+
+  if (!s[group]) s[group] = {}
+  if (!s[group][subgroup]) s[group][subgroup] = {}
+  if (!s[group][subgroup][meta.name]) s[group][subgroup][meta.name] = {}
+  const p = s[group][subgroup][meta.name]
+  
+  if (meta.variant) {
+    if (!p.variants) p.variants = {}
+    p.variants[meta.variant] = data
+  } else {
+    s[group][subgroup][meta.name] = data  
+  }
+}
+
+async function main (ver = EMOJI_VERSION) {
+  const text = await getTestFile(ver)
+  const version = parseFloat(ver)
 
   console.log(`Format text to json...`)
+  let lineNo = 0
   const collected = text.trim().split('\n').reduce((accu, line) => {
     if (line.startsWith('# group: ')) {
       console.log(`  Processing ${line.substr(2)}...`)
@@ -19,19 +42,24 @@ async function main () {
     } else if (line.startsWith('#')) {
       accu.comments = accu.comments + line + '\n'
     } else {
-      const meta = parseLine(line)
+      const meta = parseLine(line, lineNo++)
+      
       if (meta) {
+        if (meta.version > version) return accu
+
         meta.category = `${accu.group} (${accu.subgroup})`
         meta.group = accu.group
         meta.subgroup = accu.subgroup
+
         accu.full.push(meta)
         accu.compact.push(meta.char)
+        addStructured(accu, meta)         
       } else {
         accu.comments = accu.comments.trim() + '\n\n'
       }
     }
     return accu
-  }, { comments: '', full: [], compact: [] })
+  }, { comments: '', full: [], compact: [], structured: {} })
 
   console.log(`Processed emojis: ${collected.full.length}`)
 
@@ -42,9 +70,9 @@ async function main () {
 }
 
 async function getTestFile (ver) {
-  const url = `https://unicode.org/Public/emoji/${ver}/emoji-test.txt`
+  const url = `https://unicode.org/Public/emoji/${EMOJI_VERSION}/emoji-test.txt`
 
-  process.stdout.write(`Fetch emoji-test.txt (v${EMOJI_VERSION})`)
+  process.stdout.write(`Fetch emoji-test.txt (v${ver}) from ${url}`)
   return new Promise((resolve, reject) => {
     https.get(url, res => {
       let text = ''
@@ -62,7 +90,7 @@ async function getTestFile (ver) {
   })
 }
 
-function parseLine (line) {
+function parseLine (line, lineNo) {
   const data = line.trim().split(/\s+[;#] /)
 
   if (data.length !== 3) {
@@ -70,14 +98,14 @@ function parseLine (line) {
   }
 
   const [ codes, status, charAndName ] = data
-  const [ , char, name ] = charAndName.match(/^(\S+) E\d+\.\d+ (.+)$/)
-
-  return { codes, char, name }
+  const [ , char, version, name, variant ] = charAndName.match(/^(\S+) E(\d+\.\d+) (.+?)(?:\: (.*))?$/)
+  return { codes, char, version: parseFloat(version), name, variant, key: lineNo }
 }
 
 const rel = (...args) => path.resolve(__dirname, ...args)
 
-function writeFiles({ full, compact }) {
+function writeFiles({ full, compact, structured }) {
   fs.writeFileSync(rel('../emoji.json'), JSON.stringify(full), 'utf8')
   fs.writeFileSync(rel('../emoji-compact.json'), JSON.stringify(compact), 'utf8')
+  fs.writeFileSync(rel('../emoji-structured.json'), JSON.stringify(structured), 'utf8')
 }
